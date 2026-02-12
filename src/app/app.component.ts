738eeefa132e4b020aa7db66271fa262fbb11b3c
pardoma <pardoma@uji.es>
2026-02-12 13:13:51 +0100

    feat: Add Release Notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent, FooterComponent } from '@eo4geo/ngx-bok-utils';
 import { MenuItem } from 'primeng/api';
 import { Router, RouterOutlet } from '@angular/router';
 import { PdfService } from './services/pdf.service';
+import { ReleaseNotesComponent } from './components/releaseNotes/releaseNotes.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ReleaseNotesComponent],
 })
 export class AppComponent {
   headerItems: MenuItem[] = [
@@ -98,6 +99,8 @@ export class AppComponent {
       ],
     },
   ];
+
+  releaseNotes: boolean = false;
 
   constructor(private router: Router, private pdfService: PdfService) {}
 
